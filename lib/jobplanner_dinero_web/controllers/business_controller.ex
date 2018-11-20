d6e159a677b4d116f52a3bc1deb7a9b12deb62c7
Niels Sandholt Busch <niels.busch@gmail.com>
2018-11-20 20:51:56 +0100

    Add authorization for detail pages

--- lib/jobplanner_dinero_web/controllers/business_controller.ex
+++ lib/jobplanner_dinero_web/controllers/business_controller.ex
@@ -1,7 +1,9 @@
 defmodule JobplannerDineroWeb.BusinessController do
   use JobplannerDineroWeb, :controller
+  alias JobplannerDinero.Repo
 
-  plug JobplannerDineroWeb.Plugs.AuthenticateUser when action in [:index, :new, :create, :show, :edit, :update, :delete]
+  plug(JobplannerDineroWeb.Plugs.AuthenticateUser when action in [:index, :show])
+  plug(:authorize_user when action in [:show])
 
   def index(conn, _params) do
     render(conn, "index.html")
@@ -11,4 +13,27 @@ defmodule JobplannerDineroWeb.BusinessController do
     redirect(conn, to: "/")
   end
 
+  defp authorize_user(conn, _params) do
+    %{params: %{"id" => business_id}, assigns: %{current_user: current_user}} = conn
+
+    res =
+      Repo.query(
+        "select * from account_users_businesses where user_id = $1 and business_id = $2",
+        [
+          current_user.id,
+          String.to_integer(business_id)
+        ]
+      )
+
+    case res do
+      {:ok, %{num_rows: 1}} ->
+        conn
+
+      _ ->
+        conn
+        |> put_flash(:error, "You are not authorized to access that page")
+        |> redirect(to: business_path(conn, :index))
+        |> halt()
+    end
+  end
 end
