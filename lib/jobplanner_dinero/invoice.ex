ce4df6c7e1aad0994a85597599a175625c2b5089
Niels Sandholt Busch <niels.busch@gmail.com>
2018-12-19 18:59:04 +0100

    Use line item data for product line

--- lib/jobplanner_dinero/invoice.ex
+++ lib/jobplanner_dinero/invoice.ex
@@ -25,20 +25,25 @@ defmodule JobplannerDinero.Invoice do
     |> Repo.insert()
   end
 
-  def to_dinero_invoice(%Invoice{} = _invoice, contact_id) do
+  def to_dinero_invoice(%Invoice{invoice: invoice}, contact_id) do
     %Dinero.DineroInvoice{
       ContactGuid: contact_id,
       Date: Date.utc_today(),
-      ProductLines: [
-        %Dinero.DineroProductLine{
-          BaseAmountValue: 20.0,
-          Quantity: 5.0,
-          AccountNumber: 1000,
-          Description: "Standard rengøring",
-          LineType: "Product",
-          Unit: "session"
-        }
-      ]
+      ProductLines: Enum.flat_map(invoice["visits"], fn visit ->
+        Enum.map(visit["line_items"], &line_item_to_product_line/1)
+      end)
+    }
+  end
+
+  defp line_item_to_product_line(line_item) do
+    %Dinero.DineroProductLine{
+      BaseAmountValue: Map.get(line_item, "unit_cost"),
+      Quantity: Map.get(line_item, "quantity"),
+      AccountNumber: 1000,
+      Description: Map.get(line_item, "name"),
+      Comments: Map.get(line_item, "description"),
+      LineType: "Product",
+      Unit: "session"
     }
   end
 end
