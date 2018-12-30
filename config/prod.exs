66a9d57d5448ee2a457493258d0ee3ad5b2fc372
Niels Sandholt Busch <niels.busch@gmail.com>
2018-12-30 14:47:47 +0100

    Configurable port

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :jobplanner_dinero, JobplannerDineroWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "dinero.myjobplanner.com", port: 80]
-  # cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "dinero.myjobplanner.com", port: 80],
+  # cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [port: System.get_env("PORT") || 4000]
 
 # Do not print debug messages in production
 config :logger, level: :debug
