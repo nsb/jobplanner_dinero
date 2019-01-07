81feb10ea7f31dbfbce91f2156e2581b04aed9e3
Niels Sandholt Busch <niels.busch@gmail.com>
2019-01-07 19:11:26 +0100

    remove cache digest

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :jobplanner_dinero, JobplannerDineroWeb.Endpoint,
   url: [scheme: "https", host: "dinero.myjobplanner.com", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # server: true
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  # cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
