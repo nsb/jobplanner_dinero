aa1dd949992aa8afb91f3121747c3907de047d83
Niels Sandholt Busch <niels.busch@gmail.com>
2019-01-07 18:46:55 +0100

    fix syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :jobplanner_dinero, JobplannerDineroWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   # server: true
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
