1e9996e999893d978f989d3a10d685c62be69014
Niels Sandholt Busch <niels.busch@gmail.com>
2019-01-10 22:22:22 +0100

    Revert "Relate invoice with business jobplanner id"
    This reverts commit 7d81121cf3dcf439390758c0bb3fdb5b34d4c3d9.

--- priv/repo/migrations/20181101143600_create_invoices.exs
+++ priv/repo/migrations/20181101143600_create_invoices.exs
@@ -5,7 +5,8 @@ defmodule JobplannerDinero.Repo.Migrations.CreateInvoices do
     create table(:invoices) do
       add :dinero_id, :string
       add :invoice, :map
-      add :business_id, references(:account_businesses, [column: :jobplanner_id]), null: false
+      add :business_id, references(:account_businesses), null: false
+
       timestamps()
     end
   end
